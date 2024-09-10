0a23458df6672c494c1520d69361e88d6a3c9ee3
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-09-10 18:30:30 -0300

    atualizando código

--- exercicio004/common.py
+++ exercicio004/common.py
@@ -4,16 +4,24 @@ class ContaBancaria:
     limite: float
 
     def __init__(self, titular, saldo, limite):
-        self.titular: titular
-        self.saldo: saldo
-        self.limite: limite
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
 
-    def depositar(self):
-        depositando += self.saldo
+    def depositar(self, valor):
+        self.saldo += valor
+        return f'Deposito de R${valor} feito com sucesso!'
 
-    def sacar(self):
-        sacando -= self.saldo
-        if sacando > self.limite:
+    def sacar(self, valorDeSaque):
+        if  valorDeSaque  > self.limite:
             return 'Excedou o Limite da sua Conta!'
+        if  valorDeSaque > self.saldo:
+            return 'Saldo Insuficiente!'
+        self.saldo -= valorDeSaque
+        return f'Saque de {valorDeSaque} feito com sucesso'
 
-    #def verificandoSaldo(self):
+    def verificandoSaldo(self):
+        return f'Você tem R${self.saldo} em sua conta'
+    
+    def __str__(self):
+        return f'O titular: {self.titular}\nestá com o saldo atual de {self.saldo}'
--- exercicio004/main.py
+++ exercicio004/main.py
@@ -15,37 +15,45 @@ def acao():
     print('1- Depositar')
     print('2- Sacar')
     print('3- Verificar Saldo')
+    print('4 - sair')
     op_acao = int(input())
     return op_acao
     
-
 if __name__ == '__main__':
 
     while True:
-        print('Bem vindo(a) a sua conta bancaria!')
-        print('1-Entrar na conta | 2-Ops, não sei o que estou fazendo aqui!')
+        print('Bem vindo(a) a sua conta bancaria!\n1-Entrar na conta | 2-Ops, não sei o que estou fazendo aqui!\n')
         op = int(input())
         try:
             if op == 1:
                 dados = menuDeEntrada()
-                print(f'entrou na conta! Seja Bem-vindo(a) {dados.titular}')
+                print(f'entrou na conta! Seja Bem-vindo(a) {dados.titular}\n')
+                break
             elif op == 2:
                 break
+            else:
+                print('[ERRO] Só há duas opções [1,2]')
         except ValueError:
-            print('É para escolher entre 1 e 2')
+            print('[ERRO] Algarismo errado! só pode entra [1,2]')
 
-    if dados:
+    if dados: #se 'dados' contiver uma string não vazia, execute:
         while True:
             acao_titular = acao()
             try:
                 if acao_titular == 1:
-                    deposito = float(input('Qaunto você quer depositar? '))
-                    dados.depositar(deposito)
+                    valor = float(input('Quanto você quer depositar?\n'))
+                    print(dados.depositar(valor))
                 elif acao_titular == 2:
-                    saque = float(input('Quanto voce gostaria de sacar? '))
-                    dados.sacar(saque)
+                    saque = float(input('Quanto voce gostaria de sacar?\n'))
+                    print(dados.sacar(saque))   
+                elif acao_titular == 3:
+                    print(dados.verificandoSaldo())
+                elif acao_titular == 4:
+                    print(dados)
+                else:
+                    print('[ERRO] Só há duas opções [1,2]')
             except ValueError:
-                print('É para escolher entre [1,3]')
+                print('[ERRO] Algarismo errado! só pode entra [1,2]')
 
 
                 
