dc79be218cefd1f2f4e53985d6f3077314ab4ec5
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-09-11 01:06:03 -0300

    mudando nome do módulo

--- exercicio005/exercicio005.py
+++
@@ -1,4 +0,0 @@
-'''
-Classe Retangulo: Crie uma classe Retangulo que tenha os atributos largura e altura. 
-Adicione métodos para calcular a área e o perímetro do retângulo. Crie uma instância da classe e exiba os valores da área e do perímetro.
-'''
