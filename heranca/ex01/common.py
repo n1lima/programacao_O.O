c0eedcde8d36453c3c2f123186509d92c969f866
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-09-14 17:02:42 -0300

    add ex01 de herança e encapsulamento

--- heranca/ex01/common.py
+++ heranca/ex01/common.py
@@ -1,5 +1,5 @@
 class Animal:
-    __nome: set
+    __nome: str
     __idade: int
     __especie: str
 
@@ -8,6 +8,7 @@ class Animal:
         self.__idade = idade
         self.__especie = especie 
 
+    #leitura dos atributos privado
     def get_nome(self):
         return self.__nome
     
@@ -17,12 +18,57 @@ class Animal:
     def get_especie(self):
         return self.__especie
     
+    #atribuição para os atributos privado
+    def set_nome(self, nome):
+        self.__nome = nome
+    
+    def set_idade(self, idade):
+        self.__idade = idade
+    
+    def set_especie(self, especie):
+        self.__especie = especie
+
+    #print dos atributos privado
+    def __str__(self):
+        info = (f'O nome do animal: {self.get_nome()}\n'
+               f'A idade: {self.get_idade()}\n'
+               f'E sua especie: {self.get_especie()}')
+        return info
+    
 class Mamifero (Animal):
-    def tipo_de_pelo(self):
+    def __init__(self, nome, idade, especie, tipoDePelo):
+        super().__init__(nome, idade, especie)#recebendo os atributos da Classe Pai -> Animal
+        self._tipoDePelo  = tipoDePelo 
 
+    #leitura dos atributos privado
+    def get_tipoDePelo(self):
+        return self._tipoDePelo
 
+    #atribuição para os atributos privado
+    def set_tipoDePelo(self, tipoDePelo):
+        self._tipoDePelo = tipoDePelo
+
+    def __str__(self):
+        info = super().__str__()#recebendo o 'print()' da Classe Pai -> Animal
+        info += f'\ntipo de Pelo: {self.get_tipoDePelo()}'
+        return info 
+    
 class Ave (Animal):
-    def tipo_de_ninho(self):
+    def __init__(self, nome, idade, especie, tipoDeNinho):
+        super().__init__(nome, idade, especie)
+        self._tipoDeNinho = tipoDeNinho
+
+    def get_tipoDeNinho(self):
+        return self._tipoDeNinho
+    
+    def set_tipoDeNinho(self, tipoDeNinho):
+        self._tipoDeNinho = tipoDeNinho
+
+    def __str__(self):
+        info = super().__str__()
+        info += f'\ntipo de ninho: {self.get_tipoDeNinho()}'
+        return info
+
 
         
 
--- heranca/ex01/main.py
+++ heranca/ex01/main.py
@@ -5,4 +5,49 @@ Crie uma classe base chamada Animal com atributos protegidos como nome, idade e 
 Crie subclasses Mamifero e Ave que herdam de Animal. Adicione atributos e métodos específicos para cada uma das subclasses, como tipo_de_pelo para Mamifero e tipo_de_ninho para Ave.
 Implemente métodos para exibir informações detalhadas sobre cada animal, garantindo que a encapsulação seja mantida.
 """
+from common import *
 
+def menu():
+    nome = input('digite o nome do seu bichinho: ')
+    idade = int(input('digite a idade do seu bichinho: '))
+    especie = input('digite a especie do seu bichinho: ')
+    return nome, idade, especie
+
+def tipoDeAnimal():
+    try:
+        op = int(input('Qual é o tipo de aimal que você quer entrar 1- Mamifero | 2- Ave'))
+        return op
+    except ValueError:
+        print('Digite número inteiro entra [1,2]')
+        return None
+    
+if __name__ == '__main__':
+  
+    while True:
+        try:
+            inseir = input('Você quer inseir um bichinho? [SIM/NAO]').strip().upper()
+            if inseir == 'SIM':
+                nome, idade, especie = menu()
+
+                op = tipoDeAnimal()
+                
+                if op == 1:
+                    tipoDePelo = input('Digite o tipo de pelo: ')
+                    informacoesDoAnimal = Mamifero(nome, idade, especie, tipoDePelo)
+                elif op == 2:
+                    tipoDeNinho = input('Digite o tipo de ninho: ')
+                    informacoesDoAnimal = Ave(nome, idade, especie, tipoDeNinho)
+                else:
+                    print('Digite apenas 1 e 2')
+                    continue
+
+                print(informacoesDoAnimal)
+                continue
+
+            elif inseir == 'NAO':
+                print('saindo...')
+            break
+        except ValueError:
+            print('Digite apenas SIM ou NAO')
+
+    
